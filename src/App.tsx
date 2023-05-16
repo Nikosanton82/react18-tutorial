8841b53b08568ccf78067b66d01e100503ae75d0
Nikolaos Antonakakis <nikosanton82@gmail.com>
2023-05-16 13:31:00 +0200

    Exercise - Building an Expandable Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
 import { useState } from "react";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "Product 2", quantity: 1 },
-    ],
-  });
-
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
-      ),
-    });
-  };
   return (
-    <div>
-      {cart.items.map((item) => (
-        <div key={item.id}>
-          <p>{item.title}</p>
-          <p>Quantity: {item.quantity}</p>
-        </div>
-      ))}
-      <button onClick={handleClick}>Click Me</button>
-    </div>
+    <>
+      <ExpandableText>Hello World</ExpandableText>
+    </>
   );
 }
 
