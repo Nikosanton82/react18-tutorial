b51bcc57b699fc42fca7ba8e683e586acc378758
Nikolaos Antonakakis <nikosanton82@gmail.com>
2023-05-16 11:35:48 +0200

    Updating Objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import Message from "./Message";
+import { useState } from "react";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+
+  const handleClick = () => {
+    setDrink({ ...drink, price: 6 });
+  };
   return (
     <div>
-      <Message />
+      {drink.price}
+      <button onClick={handleClick}> Click Me</button>
     </div>
   );
 }
