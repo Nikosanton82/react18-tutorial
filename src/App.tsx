d2616aa4c9a35bd0d9aa7a99121ce300587598da
Nikolaos Antonakakis <nikosanton82@gmail.com>
2023-05-16 10:35:29 +0200

    Understanding the State Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import Like from "./components/Like/Like";
+import { useState } from "react";
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClick = () => {
+    setIsVisible(true);
+    console.log(isVisible);
+  };
   return (
     <div>
-      <Like onClick={() => console.log("clicked")} />
+      <button onClick={handleClick}>Show</button>
     </div>
   );
 }
