5e9896dd380bea5199f093c3bfcdde7ee6fe9c3b
Nikolaos Antonakakis <nikosanton82@gmail.com>
2023-05-17 15:13:50 +0200

    Handling form submission & accessing input fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
 import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <>
-      <ExpandableText>Hello World</ExpandableText>
+      <Form />
     </>
   );
 }
