c3ed39f1c5b107f3dcd46ace3361cbaac90f65be
Nikolaos Antonakakis <nikosanton82@gmail.com>
2023-05-17 19:07:20 +0200

    Building the Expense Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import ExpandableText from "./components/ExpandableText";
 import Form from "./components/Form";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import { useState } from "react";
+
+export const categories = ["Groceries", "Utilities", "Entertainment"];
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -19,6 +22,9 @@ function App() {
 
   return (
     <>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
       <div className="mb-3">
         <ExpenseFilter
           onSelectCategory={(category) => setSelectedCategory(category)}
