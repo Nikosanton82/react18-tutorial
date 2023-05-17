07217165045b34981ff41aeff1304c89ddb8a977
Nikolaos Antonakakis <nikosanton82@gmail.com>
2023-05-17 18:44:30 +0200

    Building ExpenseList

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import ExpandableText from "./components/ExpandableText";
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import { useState } from "react";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 20, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 30, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 20, category: "Utilities" },
+  ]);
   return (
     <>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </>
   );
 }
